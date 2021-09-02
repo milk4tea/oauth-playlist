2c2e44384eeab7e55ee950c0d80a7559f43939d0
Jeremy Feng <milk4tea@gmail.com>
2021-09-02 11:22:36 +1000

    save user to db

---
+++ config/passport-setup.js
@@ -0,0 +1,30 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./keys')
+const User = require('../models/user-model');
+
+passport.use(
+    new GoogleStrategy({
+        // options for the google strategy
+        callbackURL: '/auth/google/redirect',
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser) {
+                // already have the user
+                console.log('user is: ' + currentUser);
+            } else {
+                // if not, create user in our db
+                new User({
+                    username: profile.displayName,
+                    googleId: profile.id,
+                    email: profile.emails[0].value
+                }).save().then((newUser) => {
+                    console.log('new user created: ' + newUser);
+                });
+
+            }
+        })
+    })
+);
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 // auth login
 router.get('/login', (req, res) => {
@@ -12,9 +13,13 @@ router.get('/logout', (req, res) => {
 });
 
 // auth with goole
-router.get('/google', (req, res) => {
-    // handle with passport
-    res.send('logging in with google');
+router.get('/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+// callback route for google to redirect to 
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+    res.send('you reached the callback URI');
 });
 
 module.exports = router;
